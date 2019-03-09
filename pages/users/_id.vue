7f10a099796d32301fff65ce78d86955ad30b8e1
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2019-03-09 21:21:55 +0900

    動的ルーティングのコンテンツのだし分け

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <p>/user/_id.vue</p>
+    <p>User ID: {{userId}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    };
+  }
+};
+</script>
